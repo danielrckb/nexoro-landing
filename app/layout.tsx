dabffe57420d7a39bc8ae7a88142d4d00dcd8870
Daniel Rockenschaub <daniel.rockenschaub@outlook.com>
2025-12-09 16:34:52 +0100

    gradient fix and scroll and overflow x hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="overflow-x-hidden">
-      <body
+      <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
